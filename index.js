721ac339cab03ea113777ed0b72805617d2e3e1f
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2021-11-04 18:51:00 +0530

    packages

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
- 
+const PORT = 8000;
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const app = express();
+
+app.listen(PORT, () => console.log("Server is running in PORT ${PORT}"));
