3a9d770bca1f40da083651eb7e7c6f82fbaf328e
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2021-11-04 20:50:53 +0530

    mid

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ const PORT = 8000;
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const { response } = require("express");
 
 const app = express();
 
-app.listen(PORT, () => console.log("Server is running in PORT ${PORT}"));
+const articles = [];
+
+app.get("/", (req, res) => {
+  res.json("Welcome to CLimate change news api");
+});
+
+app.get("/news/", (req, res) => {
+  axios
+    .get("https://www.theguardian.com/environment/climate-crisis")
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      $('a:contains("climate")', html).each(function () {
+        const title = $(this).text();
+        const url = $("this").attr("href");
+        articles.push({ title: title, url: url });
+      });
+      res.json(articles);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.listen(PORT, () => console.log("Server is running in PORT", PORT));
