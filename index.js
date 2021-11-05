2d3e8a288b490bb26174a8a7b92ad49a5c8d9567
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2021-11-05 19:10:55 +0530

    url

--- index.js
+++ index.js
@@ -6,14 +6,48 @@ const { response } = require("express");
 
 const app = express();
 
+const newspapers = [
+  {
+    name: "thetimes",
+    address: "https://www.thetimes.co.uk/environment/climate-change",
+    base: "",
+  },
+  {
+    name: "guardian",
+    address: "https://www.theguardian.com/environment/climate-crisis",
+    base: "",
+  },
+  {
+    name: "telegraph",
+    address: "https://www.telegraph.co.uk/climate-change",
+    base: "https://www.telegraph.co.uk/",
+  },
+];
+
 const articles = [];
+
+newspapers.forEach((newspaper) => {
+  axios.get(newspaper.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    $('a:contains("climate")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr("href");
+      articles.push({
+        title: title,
+        url: newspaper.base + url,
+        source: newspaper.name,
+      });
+    });
+  });
+});
 
 app.get("/", (req, res) => {
   res.json("Welcome to CLimate change news api");
 });
 
 app.get("/news/", (req, res) => {
-  axios
+  /*axios
     .get("https://www.theguardian.com/environment/climate-crisis")
     .then((response) => {
       const html = response.data;
@@ -26,7 +60,14 @@ app.get("/news/", (req, res) => {
       });
       res.json(articles);
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err));*/
+  res.json(articles);
+});
+
+app.get("/news/:newspaperId", async (req, res) => {
+  console.log(req);
+  newspapers.filter((newspaper) => newspaper.name == newspaperId);
+  axios.get();
 });
 
 app.listen(PORT, () => console.log("Server is running in PORT", PORT));
